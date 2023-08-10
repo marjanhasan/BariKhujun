a04c7787fb9a8d8b2402745668bec31b8cf2f36a
Kazi Fahim Imam <kazi.fahim@pathao.com>
2023-08-10 19:15:49 +0600

    Blank

--- Server/controllers/publicController.js
+++ Server/controllers/publicController.js
@@ -1,68 +1,12 @@
 const House = require("../models/houseModel");
-let pageSize = 5
 const handleRouteError = require("../utils/errorHandler");
+const {handleGetHouse, handleGetHouses} = require("../utils/utils");
 
 async function getHouses (req, res) {
-    try {
-        const page = parseInt(req.query.page) || 1;
-        const skip = (page - 1) * pageSize;
-        let givenPageSize = parseInt(req.query.pageSize);
-        if (!isNaN(givenPageSize)) {
-            pageSize = givenPageSize
-        }
-        // Count the total number of houses
-        const totalHouses = await House.countDocuments();
-
-        // Count total page
-        let totalPage = Math.ceil(totalHouses / pageSize)
-        if (page > totalPage) {
-            res.status(404).send({
-                success: false,
-                message: "Page number not valid",
-                totalPages: totalPage,
-            });
-            return;
-        }
-        // Get the paginated houses
-        const houses = await House.find().limit(pageSize).skip(skip);
-
-        if (houses.length > 0) {
-            res.status(200).send({
-                success: true,
-                message: "Returned houses",
-                data: houses,
-                meta: {
-                    "Current Page": page,
-                    "Total Page": totalPage,
-                }
-            });
-        } else {
-            res.status(404).send({
-                success: false,
-                message: "Houses not found",
-            });
-        }
-    } catch (error) {
-        handleRouteError(res, error, null, null)
-    }
+    await handleGetHouses(req, res)
 }
 async function getHouse (req, res) {
-    try {
-        const house = await House.findById(req.params.id)
-        if (!house) {
-            return res.status(404).send({
-                success: false,
-                message: "House not found with this id",
-            });
-        }
-        res.status(200).send({
-            success: true,
-            message: "House Retrieved",
-            data: house,
-        });
-    } catch (error) {
-        handleRouteError(res, error, null, null)
-    }
+    await handleGetHouse(req, res)
 }
 
 module.exports = {getHouses, getHouse}
--- Server/utils/utils.js
+++ Server/utils/utils.js
@@ -3,6 +3,7 @@ const House = require("../models/houseModel");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+let pageSize = 5
 const handleAddHouse = async (req, res, houseData) => {
     try {
         if (houseData.email) {
@@ -23,6 +24,68 @@ const handleAddHouse = async (req, res, houseData) => {
         handleRouteError(res, error, null, null);
     }
 };
+const handleGetHouses = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const skip = (page - 1) * pageSize;
+        let givenPageSize = parseInt(req.query.pageSize);
+        if (!isNaN(givenPageSize)) {
+            pageSize = givenPageSize
+        }
+        // Count the total number of houses
+        const totalHouses = await House.countDocuments();
+
+        // Count total page
+        let totalPage = Math.ceil(totalHouses / pageSize)
+        if (page > totalPage) {
+            res.status(404).send({
+                success: false,
+                message: "Page number not valid",
+                totalPages: totalPage,
+            });
+            return;
+        }
+        // Get the paginated houses
+        const houses = await House.find().limit(pageSize).skip(skip);
+
+        if (houses.length > 0) {
+            res.status(200).send({
+                success: true,
+                message: "Returned houses",
+                data: houses,
+                meta: {
+                    "Current Page": page,
+                    "Total Page": totalPage,
+                }
+            });
+        } else {
+            res.status(404).send({
+                success: false,
+                message: "Houses not found",
+            });
+        }
+    } catch (error) {
+        handleRouteError(res, error, null, null)
+    }
+}
+const handleGetHouse = async (req, res) => {
+    try {
+        const house = await House.findById(req.params.id)
+        if (!house) {
+            return res.status(404).send({
+                success: false,
+                message: "House not found with this id",
+            });
+        }
+        res.status(200).send({
+            success: true,
+            message: "House Retrieved",
+            data: house,
+        });
+    } catch (error) {
+        handleRouteError(res, error, null, null)
+    }
+}
 const handleUpdateHouse = async (req, res, houseData) => {
     try {
         const house = houseData;
@@ -52,6 +115,25 @@ const handleUpdateHouse = async (req, res, houseData) => {
         handleRouteError(res, error, null, null)
     }
 };
+const handleDeleteHouse = async (req, res) => {
+    try {
+        const house = await House.findByIdAndDelete(req.params.id);
+        if (!house) {
+            return res.status(404).send({
+                success: false,
+                message: "House not found with this id",
+            });
+        }
+
+        res.status(200).send({
+            success: true,
+            message: "House deleted",
+            data: house,
+        });
+    } catch (error) {
+        handleRouteError(res, error, null, null);
+    }
+}
 const handleRegisterUser = async (req, res, userData) => {
     try {
         const user = await User.findOne({
@@ -96,7 +178,10 @@ const handleRegisterUser = async (req, res, userData) => {
 };
 
 module.exports = {
+    handleGetHouse,
+    handleGetHouses,
     handleAddHouse,
     handleUpdateHouse,
+    handleDeleteHouse,
     handleRegisterUser,
 };

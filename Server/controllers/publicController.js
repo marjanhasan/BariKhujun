41c71877029ef4d9f64339302d0c2881c3224c8c
Kazi Fahim Imam <kazi.fahim@pathao.com>
2023-08-11 22:48:32 +0600

    Remove Forbidden request

--- Server/controllers/publicController.js
+++ Server/controllers/publicController.js
@@ -1,6 +1,10 @@
 const House = require("../models/houseModel");
 const handleRouteError = require("../utils/errorHandler");
 const {handleGetHouse, handleGetHouses, handleRegisterUser} = require("../utils/utils");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 
 async function getHouses (req, res) {
     await handleGetHouses(req, res)
@@ -11,4 +15,53 @@ async function getHouse (req, res) {
 async function registerUser (req, res) {
     await handleRegisterUser(req, res, req.body)
 }
-module.exports = {getHouses, getHouse, registerUser}
+
+async function loginUser (req, res) {
+    try {
+        const user = await User.findOne({
+            $or: [{ email: req.body.email }, { username: req.body.username }],
+        });
+
+        if (!user) {
+            return res.status(401).send({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).send({
+                success: false,
+                message: "Incorrect password",
+                data: {
+                    desc: "Provide valid password"
+                },
+            });
+        }
+
+        // User authentication successful, generate access token
+        const accessToken = jwt.sign({ id: user._id, name: user.name }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1h', // Access token expires in 1 hour
+        });
+
+        // Generate refresh token
+        const refreshToken = jwt.sign({ id: user._id, name: user.name }, process.env.REFRESH_TOKEN_SECRET, {
+            expiresIn: '1d', // Refresh token expires in 7 days
+        });
+
+        res.status(200).send({
+            success: true,
+            message: "User is logged in successfully",
+            data: {
+                "access-token": "Bearer " + accessToken,
+                "refresh-token": "Bearer " + refreshToken,
+
+            },
+        });
+    } catch (error) {
+        handleRouteError(res, error,null,null);
+    }
+}
+module.exports = {getHouses, getHouse, registerUser, loginUser}
--- Server/routes/publicRoutes.js
+++ Server/routes/publicRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const {getHouses, getHouse, registerUser} = require("../controllers/publicController");
+const {getHouses, getHouse, registerUser, loginUser} = require("../controllers/publicController");
 const {handleValidationErrors, validateRegisterInput} = require("../middleware/validateUserInput");
 
 // User Registration
 router.post("/register", validateRegisterInput, handleValidationErrors, registerUser);
+// User Login
+router.post("/login", validateLoginInput, handleValidationErrors, loginUser);
 // Get houses with pagination
 router.get("/houses", getHouses);
 //Get a single house by ID
